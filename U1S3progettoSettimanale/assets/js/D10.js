ea5ed570721f3fe89f951a65065922e6287dd7f7
b-bauld <smoulderpipe@gmail.com>
2024-02-09 21:50:09 +0100

    checkpoint es.15

--- U1S3progettoSettimanale/assets/js/D10.js
+++ U1S3progettoSettimanale/assets/js/D10.js
@@ -98,7 +98,7 @@ dice();
   Crea una funzione chiamata "whoIsBigger" che riceve due numeri come parametri e ritorna il maggiore dei due.
 */
 
-function whoIsBigger(number1, number2){
+function whoIsBigger(number1, number2) {
   if (number1 > number2) {
     console.log(number1 + " è maggiore di " + number2);
   } else if (number2 > number1) {
@@ -418,35 +418,65 @@ const movies = [
   Scrivi una funzione chiamata "newestMovie" che trova il film più recente nell'array "movies" fornito.
 */
 
-  function piuVecchio() {
-    let anno = 1200;
-    movies.forEach((element) => {
-      //Number trasforma un valore di tipo stringa, booleano o altro in un numero.
-      //se l'anno è inferiore a 2100
-      //anno corrisponde al valore riportato nella proprietà year degli oggetti dell'array
-      if (Number(element.Year) > anno) {
-        anno = Number(element.Year);
-        titolo = element.Title;
-      }
-    });
-    console.log(`il film più recente è del ${anno} e si chiama "${titolo}"`);
-  }
-  console.log('****** ESERCIZIO 12 ******');
-  piuVecchio();
+function piuVecchio() {
+  let anno = 1200;
+  movies.forEach((element) => {
+    //Number trasforma un valore di tipo stringa, booleano o altro in un numero.
+    //se l'anno è superiore a 1200
+    //anno corrisponde al valore riportato nella proprietà year degli oggetti dell'array
+    if (Number(element.Year) > anno) {
+      anno = Number(element.Year);
+      titolo = element.Title;
+    }
+  });
+  console.log(`Il film più recente dell'array movies è del ${anno} e si intitola "${titolo}"`);
+}
+console.log('****** ESERCIZIO 12 ******');
+piuVecchio();
 
-  
+
 
 /* ESERCIZIO 13
   Scrivi una funzione chiamata countMovies che ritorna il numero di film contenuti nell'array "movies" fornito.
 */
 
+function countMovies() {
+  console.log(`Nell'array "movies" ci sono in totale ${movies.length} film.`);
+}
+console.log('****** ESERCIZIO 13 ******');
+countMovies();
+
 /* ESERCIZIO 14
   Scrivi una funzione chiamata "onlyTheYears" che crea un array con solamente gli anni di uscita dei film contenuti nell'array "movies" fornito.
 */
 
+function onlyTheYears() {
+  let yearsList = [];
+  for (let i = 0; i < movies.length; i++) {
+    yearsList.push(movies[i].Year);
+  }
+  console.log(yearsList);
+
+}
+console.log('****** ESERCIZIO 14 ******');
+onlyTheYears();
+
 /* ESERCIZIO 15
   Scrivi una funzione chiamata "onlyInLastMillennium" che ritorna solamente i film prodotto nel millennio scorso contenuti nell'array "movies" fornito.
 */
+
+
+
+let onlyInLastMillennium = () =>
+movies.forEach((element) => {
+  if (Number(element.Year) < 2000) {
+    console.log(element);
+  }
+});
+
+console.log('****** ESERCIZIO 15 ******');
+console.log('i film prodotti nel millennio scorso sono:');
+onlyInLastMillennium();
 
 /* ESERCIZIO 16
   Scrivi una funzione chiamata "sumAllTheYears" che ritorna la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array "movies" fornito.
@@ -527,118 +557,3 @@ const movies = [
 
 /* Questo array viene usato per gli esercizi. Non modificarlo. */
 
-// const movies = [
-//   {
-//     Title: 'The Lord of the Rings: The Fellowship of the Ring',
-//     Year: '2001',
-//     imdbID: 'tt0120737',
-//     Type: 'movie',
-//     Poster:
-//       'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg',
-//   },
-
-//   {
-//     Title: 'The Lord of the Rings: The Return of the King',
-//     Year: '2003',
-//     imdbID: 'tt0167260',
-//     Type: 'movie',
-//     Poster:
-//       'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
-//   },
-//   {
-//     Title: 'The Lord of the Rings: The Two Towers',
-//     Year: '2002',
-//     imdbID: 'tt0167261',
-//     Type: 'movie',
-//     Poster:
-//       'https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg',
-//   },
-//   {
-//     Title: 'Lord of War',
-//     Year: '2005',
-//     imdbID: 'tt0399295',
-//     Type: 'movie',
-//     Poster:
-//       'https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
-//   },
-//   {
-//     Title: 'Lords of Dogtown',
-//     Year: '2005',
-//     imdbID: 'tt0355702',
-//     Type: 'movie',
-//     Poster:
-//       'https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg',
-//   },
-//   {
-//     Title: 'The Lord of the Rings',
-//     Year: '1978',
-//     imdbID: 'tt0077869',
-//     Type: 'movie',
-//     Poster:
-//       'https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
-//   },
-//   {
-//     Title: 'Lord of the Flies',
-//     Year: '1990',
-//     imdbID: 'tt0100054',
-//     Type: 'movie',
-//     Poster:
-//       'https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg',
-//   },
-//   {
-//     Title: 'The Lords of Salem',
-//     Year: '2012',
-//     imdbID: 'tt1731697',
-//     Type: 'movie',
-//     Poster:
-//       'https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg',
-//   },
-//   {
-//     Title: 'Greystoke: The Legend of Tarzan, Lord of the Apes',
-//     Year: '1984',
-//     imdbID: 'tt0087365',
-//     Type: 'movie',
-//     Poster:
-//       'https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg',
-//   },
-//   {
-//     Title: 'Lord of the Flies',
-//     Year: '1963',
-//     imdbID: 'tt0057261',
-//     Type: 'movie',
-//     Poster:
-//       'https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg',
-//   },
-//   {
-//     Title: 'The Avengers',
-//     Year: '2012',
-//     imdbID: 'tt0848228',
-//     Type: 'movie',
-//     Poster:
-//       'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
-//   },
-//   {
-//     Title: 'Avengers: Infinity War',
-//     Year: '2018',
-//     imdbID: 'tt4154756',
-//     Type: 'movie',
-//     Poster:
-//       'https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg',
-//   },
-//   {
-//     Title: 'Avengers: Age of Ultron',
-//     Year: '2015',
-//     imdbID: 'tt2395427',
-//     Type: 'movie',
-//     Poster:
-//       'https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg',
-//   },
-//   {
-//     Title: 'Avengers: Endgame',
-//     Year: '2019',
-//     imdbID: 'tt4154796',
-//     Type: 'movie',
-//     Poster:
-//       'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg',
-//   },
-// ]
