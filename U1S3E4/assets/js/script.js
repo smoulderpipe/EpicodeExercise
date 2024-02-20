3b54682dbeed8d5a64287cf4f15c9b81953b9948
smoulderpipe <del.v.paola@gmail.com>
2024-02-20 17:17:20 +0100

    feat:U2S1E2 checkpoint esercizio giornaliero

--- U1S3E4/assets/js/script.js
+++ U1S3E4/assets/js/script.js
@@ -25,7 +25,7 @@ for (let i = 1; i <= 90; i++) {
 }
 
 //definisco una variabile e la associo all'elemento tabella presente in html
-const tableElement = document.querySelector('table');
+const tableElement = document.getElementById('tabellaTabellone');
 
 const generaTabellone = () => {
     //creo le righe da 1 a 9
@@ -45,6 +45,61 @@ const generaTabellone = () => {
 
 generaTabellone();
 
+/*
+La cartella dovrebbe seguire la stessa logica della vera cartella di una tombola:
+alcuni td dovrebbero essere privi di contenuto
+nei td con un numero al loro interno posizionati sulla stessa colonna dovrebbe corrispondere un numero entro una specifica decina
+
+(quindi, per esempio:
+- nella prima colonna dovrebbero essere presenti o td vuoti oppure td che presentano un numero compreso tra 1 e 10
+- mentre nell'ultima dovrebbero essere presenti o td vuoti oppure td che presentano un numero tra 81 e 90
+- inoltre ogni riga dovrebbe contenere 4 td vuoti e 5 contenenti un numero).
+*/
+
+// const numeriCartella = [];
+
+// for (let i = 0; i < 16; i++) {
+
+// }
+
+//genera cartelle
+const cartellaElement1 = document.getElementById('tabellaCartella1');
+const generaCartelle = () => {
+    let numeroRandomCartella;
+    for (let i = 0; i < 3; i++) {
+        const tr = document.createElement('tr');
+        for (let j = 0; j < 10; j++) {
+            const td = document.createElement('td');
+            // td.innerText = tabellone[i * 10 + j];
+            td.style.border = "1px solid red";
+            td.style.width = "20px";
+            td.style.height = "20px";
+            td.style.textAlign = "center";
+            
+            tr.appendChild(td);
+            
+    }
+    //appendo la riga alla tabella
+    cartellaElement1.appendChild(tr);
+    
+}
+}
+generaCartelle();
+
+function inserisciNumeriCartelle() {
+    
+    const celleVuoteCartellaNodeList = cartellaElement1.querySelectorAll("#tabellaCartella1 td");
+    const celleVuoteCartella = Array.from(celleVuoteCartellaNodeList);
+    console.log(celleVuoteCartella);
+    for (let i = 0; i < celleVuoteCartella.length; i++) {
+        numeroCasella = celleVuoteCartellaNodeList[i];
+        numeroCasella.innerText = Math.floor(Math.random() * tabellone.length + 1);
+        // td.innerText = tabellone[i * 10 + j];
+    }
+}
+
+inserisciNumeriCartelle();
+
 const numeroEstrattoDOM = document.getElementById("numero-estratto");
 const estraiNumeroButton = document.getElementById("estrai-numero");
 const numeriEstratti = [];
@@ -56,21 +111,34 @@ let estraiNumero = (tabellone) => {
 
     while (!estratto) {
         numeroEstratto = Math.floor(Math.random() * tabellone.length + 1);
-
+    
         // Controlla se il numero è già stato estratto
         if (numeriEstratti.includes(numeroEstratto) || numeroEstratto === 0) {
             continue; // Se sì, riprova
         } else {
-            estratto = true; // Se no, imposta il flag a true
+            estratto = true; // Se no, esci dal ciclo
         }
     }
+    
+    // for (let i = 0; i < tabellone.length; i++) {
+    //     numeroEstratto = Math.floor(Math.random() * tabellone.length + 1);
+    
+    //     // Controlla se il numero è già stato estratto
+    //     if (numeriEstratti.includes(numeroEstratto) || numeroEstratto === 0) {
+    //         continue; // Se sì, riprova
+    //     } else {
+    //         break; // Se no, esci dal ciclo
+    //     }
+    // }
+
+  
 
     numeriEstratti.push(numeroEstratto); // Aggiungi il numero estratto all'array
 
     // Aggiorna l'interfaccia utente
     numeroEstrattoDOM.innerText = numeroEstratto;
 
-    const celleNodeList = tableElement.querySelectorAll("td");
+    const celleNodeList = tableElement.querySelectorAll("#tabellaTabellone td");
     const celle = Array.from(celleNodeList);
 
     for (let i = 0; i < celle.length; i++ && celle.length > 1) {
