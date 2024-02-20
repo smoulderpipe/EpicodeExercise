77e8bf52bf5d11283e54c1164850b6ceff18f3c8
smoulderpipe <del.v.paola@gmail.com>
2024-02-20 10:36:53 +0100

    update:U1S3E4 (Tombola)

--- U1S3E4/assets/js/script.js
+++ U1S3E4/assets/js/script.js
@@ -4,7 +4,10 @@
 
 // • Dopo il click del bottone, evidenza in qualche modo la cella corrispondente sul tabellone (quella con lo stesso numero), ad esempio cambiandone il colore di sfondo.
 
-//+ eventuali EXTRA da vedere dopo, il punto 2 e punto 3 sono veramente extra, il 1 se riesci fallo
+/*
+EXTRA:
+• Fai in modo che non sia possibile richiamare un numero già precedentemente estratto.
+*/
 
 
 //creare una griglia di 90 numeri, su 9 righe, ognuna composta da 10 caselle
@@ -20,6 +23,7 @@ const tabellone = []; //un array che conterrà le caselle del tabellone
 for (let i = 1; i <= 90; i++) {
     tabellone.push(i);
 }
+
 //definisco una variabile e la associo all'elemento tabella presente in html
 const tableElement = document.querySelector('table');
 
@@ -27,31 +31,61 @@ const generaTabellone = () => {
     //creo le righe da 1 a 9
     for (let i = 0; i < 9; i++) {
         const tr = document.createElement('tr');
-    //e per ciascuna riga, creo 10 td
+        //e per ciascuna riga, creo 10 td
         for (let j = 0; j < 10; j++) {
             const td = document.createElement('td');
-    //per ciascun td, inserisco al suo interno un testo.
-    //questo testo sarà il risultato tra (1<i<9)*10 + j
-    //quindi per esempio:
-    // 0*10 + 1 = 1
-    // 1*10 + 1 = 11
-    // 2*10 + 1 = 21
-    // 3*10 + 1 = 31
-    //...
-    //0*10 + 2 = 2
-    //1*10 + 2 = 22
-    //...
-    //fino a
-    //8*10 + 10 = 90
             td.innerText = tabellone[i * 10 + j];
-    //appendo la casella alla riga
+            //appendo la casella alla riga
             tr.appendChild(td);
         }
-    //appendo la riga alla tabella
-    tableElement.appendChild(tr);
+        //appendo la riga alla tabella
+        tableElement.appendChild(tr);
     }
 };
 
-generaTabellone(0); // Estrai e modifica la prima casella
+generaTabellone();
 
+const numeroEstrattoDOM = document.getElementById("numero-estratto");
+const estraiNumeroButton = document.getElementById("estrai-numero");
+const numeriEstratti = [];
 
+let estraiNumero = (tabellone) => {
+    // e.preventDefault();
+    let numeroEstratto;
+    let estratto = false;
+
+    while (!estratto) {
+        numeroEstratto = Math.floor(Math.random() * tabellone.length + 1);
+
+        // Controlla se il numero è già stato estratto
+        if (numeriEstratti.includes(numeroEstratto) || numeroEstratto === 0) {
+            continue; // Se sì, riprova
+        } else {
+            estratto = true; // Se no, imposta il flag a true
+        }
+    }
+
+    numeriEstratti.push(numeroEstratto); // Aggiungi il numero estratto all'array
+
+    // Aggiorna l'interfaccia utente
+    numeroEstrattoDOM.innerText = numeroEstratto;
+
+    const celleNodeList = tableElement.querySelectorAll("td");
+    const celle = Array.from(celleNodeList);
+
+    for (let i = 0; i < celle.length; i++ && celle.length > 1) {
+        if (numeroEstratto == celle[i].innerText) {
+            celle[i].style.background = "red";
+            break;
+        }
+    }
+};
+
+estraiNumeroButton.addEventListener("click", () => {
+    if (numeriEstratti.length < 90) {
+        estraiNumero(tabellone);
+    } else {
+        window.alert("Fine del gioco! Ti va di fare un'altra partita?");
+        location.reload();
+    }
+});
