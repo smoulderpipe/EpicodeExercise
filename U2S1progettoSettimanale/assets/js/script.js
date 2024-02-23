4da94c429e4294d434258b7a5b2baa7c2f98c18f
smoulderpipe <del.v.paola@gmail.com>
2024-02-23 17:57:19 +0100

    feat:U2S1progettoSettimanale - responsiveness

--- U2S1progettoSettimanale/assets/js/script.js
+++ U2S1progettoSettimanale/assets/js/script.js
@@ -1,9 +1,11 @@
 let sfondoBianco = "white-background";
 let sfondoVerde = "green-background";
 let scrollTrigger = 60;
+const heroSection = document.getElementById("hero");
+const heroHeight = heroSection.getBoundingClientRect().height;
 
   window.addEventListener("scroll", () => {
-    if (window.scrollY >= 1){
+    if (window.scrollY >= heroHeight){
         document.getElementsByTagName("header")[0].classList.add(sfondoBianco);
         // document.getElementById("get-started").style.background = 'green';
         document.getElementById("get-started").classList.add(sfondoVerde);
