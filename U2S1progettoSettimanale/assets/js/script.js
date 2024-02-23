a10d89c283bcda44d3df691ca51ed5ea1d86fc4c
smoulderpipe <del.v.paola@gmail.com>
2024-02-23 16:22:48 +0100

    fix:U2S1progettoSettimanale - header

--- U2S1progettoSettimanale/assets/js/script.js
+++ U2S1progettoSettimanale/assets/js/script.js
@@ -1,11 +1,16 @@
-var className = "white-background";
-var scrollTrigger = 60;
+let sfondoBianco = "white-background";
+let sfondoVerde = "green-background";
+let scrollTrigger = 60;
 
   window.addEventListener("scroll", () => {
     if (window.scrollY >= 1){
-        document.getElementsByTagName("header")[0].classList.add(className);
+        document.getElementsByTagName("header")[0].classList.add(sfondoBianco);
+        // document.getElementById("get-started").style.background = 'green';
+        document.getElementById("get-started").classList.add(sfondoVerde);
     } else {
-        document.getElementsByTagName("header")[0].classList.remove(className);
+        document.getElementsByTagName("header")[0].classList.remove(sfondoBianco);
+        // document.getElementById("get-started").style.background = 'black';
+        document.getElementById("get-started").classList.remove(sfondoVerde);
     }
 })
 
