ab1d4057e8b694333825dd807fe6eb9ba3a2f32e
smoulderpipe <del.v.paola@gmail.com>
2024-03-05 17:22:36 +0100

    update: U2S3E2 checkpoint esercizio 2

--- U2S3E2/assets/script.js
+++ U2S3E2/assets/script.js
@@ -18,3 +18,28 @@ function removeFromLS() {
         alert("Non ci sono nomi da rimuovere.");
     }
 }
+
+const timer = document.getElementById("timer");
+
+let startTime;
+// Controlla se il tempo è già stato memorizzato in sessionStorage
+if (sessionStorage.getItem("sessionStartTime")) {
+    startTime = parseInt(sessionStorage.getItem("sessionStartTime"));
+} else {
+// Imposta l'inizio del tempo alla sessione corrente
+    startTime = Date.now();
+    sessionStorage.setItem("sessionStartTime", startTime);
+}
+
+
+// Funzione per aggiornare il timer ogni secondo
+function updateTimer() {
+    const tempoTrascorso = Math.floor((Date.now() - startTime) / 1000);
+    timer.textContent = `${tempoTrascorso} secondi`;
+
+// Aggiorna il timer ogni secondo
+    setTimeout(updateTimer, 1000);
+}
+
+// Avvia l'aggiornamento del timer
+updateTimer();
