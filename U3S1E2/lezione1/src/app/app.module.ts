720bf54def50efbdb3cb2045c7ab62143ae9dbca
smoulderpipe <del.v.paola@gmail.com>
2024-03-19 14:58:03 +0100

    feat: implement 1st Angular Exercise

---
+++ U3S1E2/lezione1/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { Component1Component } from './components/component1/component1.component';
+import { Component2Component } from './components/component2/component2.component';
+import { Component3Component } from './components/component3/component3.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Component1Component,
+    Component2Component,
+    Component3Component
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
