c3d6aa050f19ee3cd45bfe6aade0d66d7bdcb6a4
smoulderpipe <del.v.paola@gmail.com>
2024-03-22 15:54:16 +0100

    feat: U3S1progettoSettimanale 1st commit

--- U3S1E3/esercizio/esercizio/src/app/app.module.ts
+++ U3S1E3/esercizio/esercizio/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ActivePostsComponent } from './components/active-posts/active-posts.component';
 import { InactivePostsComponent } from './components/inactive-posts/inactive-posts.component';
-import { PostDetailComponent } from './components/post-detail/post-detail.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 
@@ -13,7 +12,6 @@ import { HomeComponent } from './components/home/home.component';
     AppComponent,
     ActivePostsComponent,
     InactivePostsComponent,
-    PostDetailComponent,
     NavbarComponent,
     HomeComponent
   ],
